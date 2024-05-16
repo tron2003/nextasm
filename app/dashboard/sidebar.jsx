3009ef9ef765c55fcfb0dd59f0bc650f28dd6901
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-05-17 00:34:28 +0530

    first commit

---
+++ app/dashboard/sidebar.jsx
@@ -0,0 +1,96 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+import { GrTransaction } from "react-icons/gr";
+import { FaUserAstronaut } from "react-icons/fa";
+import { IoPricetagsOutline } from "react-icons/io5";
+import { FaUsers } from "react-icons/fa";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { TbReportSearch } from "react-icons/tb";
+import { IoBriefcaseOutline } from "react-icons/io5";
+import { RiDashboardHorizontalLine } from "react-icons/ri";
+import { IoSettingsOutline } from "react-icons/io5";
+import Image from 'next/image'
+import { LuLogOut } from "react-icons/lu";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { usePathname } from 'next/navigation';
+export default function sidebar() {
+
+
+    const pathname=usePathname()
+    console.log(pathname)
+    return (
+        <div className='text-white flex flex-col h-full max-h-full'>
+            {/* Pages */}
+            <div className="flex flex-row pb-4 pt-3 items-center gap-3">
+                <Image src="/noavatar.png" width="50" height="50" className='rounded-full  inline ' />
+                Tron 
+                
+
+            </div>
+            <pages className="">
+                <ul className='text-xs font-semibold'>Pages</ul>
+                <div className='flex flex-col p-3 text-xs font-normal'>
+                    {/* Links for Pages */}
+                    <Link href="/dashboard" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        <RiDashboardHorizontalLine className="inline " />
+                        Dashboard
+                    </Link>
+                    <Link href="/users" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        < FaUserAstronaut className='inline' />
+                        Users
+                    </Link>
+                    <Link href="/products" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        <IoPricetagsOutline className='inline' />
+                        Products
+                    </Link>
+                    <Link href="/Transactions" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        <GrTransaction className='inline' />
+                        Transactions
+                    </Link>
+                </div>
+            </pages>
+
+            {/* Analytics */}
+            <analytics className="">
+                <ul className='text-xs font-semibold'>Analytics</ul>
+                <div className='flex flex-col p-3 text-xs font-normal'>
+                    {/* Links for Analytics */}
+                    <Link href="/dashboard" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg '>
+                        <IoBriefcaseOutline className="inline " />
+                        Revenue
+                    </Link>
+                    <Link href="/users" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        <TbReportSearch className='inline' />
+                        Reports
+                    </Link>
+                    <Link href="/products" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        <FaUsers className='inline' />
+                        Teams
+                    </Link>
+                </div>
+            </analytics>
+
+            {/* User */}
+            <user className="">
+                <ul className='text-xs font-semibold'>User</ul>
+                <div className='flex flex-col p-3 text-xs font-normal'>
+                    {/* Links for User */}
+                    <Link href="/dashboard" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg '>
+                        <IoSettingsOutline className="inline " />
+                        Settings
+                    </Link>
+                    <Link href="/users" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        <IoIosHelpCircleOutline className='inline' />
+                        Help
+                    </Link>
+                    <Link href="/products" className='flex gap-2 items-center hover:bg-sky-700 p-3 rounded-lg'>
+                        <LuLogOut className='inline' />
+                        Logout
+                    </Link>
+                </div>
+            </user>
+        </div>
+
+    )
+}
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='h-full  bg-sky-950'>
-      Home
+    <div >
+      login page
     </div>
   )
 }
